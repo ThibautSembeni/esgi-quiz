45f0246eb7386dc152bf711d30913fa9d0b3d3cd
ThibautSembeni <thibaut.sbn.dev@gmail.com>
2024-02-23 22:35:59 +0100

    fix(Navbar): add conditional rendering for navigation links and login button based on user authentication status
    The changes in this commit add conditional rendering to the Navbar component in order to display navigation links and a login button based on the user's authentication status.
    
    Previously, all navigation links were always rendered, regardless of whether the user was authenticated or not. With this commit, the navigation links will only be rendered if the user is authenticated. This improves the user experience by showing relevant links based on the user's authentication status.
    
    Additionally, a login button is now conditionally rendered based on whether the user is authenticated or not. If the user is not authenticated, the login button will be displayed, allowing them to log in. If the user is already authenticated, the login button will not be shown.
    
    These changes were made to provide a more personalized and intuitive navigation experience for users of the application.

--- client/src/components/organisms/Navbar/index.tsx
+++ client/src/components/organisms/Navbar/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Dialog } from "@headlessui/react";
 import React, { useState } from "react";
+import { useAuth } from "@/context/AuthContext";
 
 const navigation = [
   { name: "Créer une fiche", href: "/card-creation" },
@@ -14,6 +15,7 @@ interface NavbarProps {
 }
 export default function Navbar({ className, children }: NavbarProps) {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const { isAuthenticated } = useAuth();
 
   return (
     <header className={`absolute inset-x-0 top-0 z-50 ${className}`}>
@@ -42,23 +44,26 @@ export default function Navbar({ className, children }: NavbarProps) {
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
-          {navigation.map((item) => (
-            <Link
-              key={item.name}
-              href={item.href}
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              {item.name}
-            </Link>
-          ))}
+          {isAuthenticated &&
+            navigation.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
+                {item.name}
+              </Link>
+            ))}
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <Link
-            href="/auth/login"
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
-            Se connecter <span aria-hidden="true">&rarr;</span>
-          </Link>
+          {!isAuthenticated && (
+            <Link
+              href="/auth/login"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Se connecter <span aria-hidden="true">&rarr;</span>
+            </Link>
+          )}
         </div>
       </nav>
       <Dialog
@@ -90,23 +95,26 @@ export default function Navbar({ className, children }: NavbarProps) {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    {item.name}
-                  </Link>
-                ))}
+                {isAuthenticated &&
+                  navigation.map((item) => (
+                    <Link
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </Link>
+                  ))}
               </div>
               <div className="py-6">
-                <Link
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </Link>
+                {!isAuthenticated && (
+                  <Link
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Log in
+                  </Link>
+                )}
               </div>
             </div>
           </div>
